0189666acf57e67bc650ab6c8610168a19b4cbd2
educia27 <edd1114@gmail.com>
2022-03-15 10:13:57 -0500

    eddy portfolio started

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,17 @@
+import "./topbar.scss"
+
+export default function Topbar() {
+  return (
+    <div className="topbar">
+        <div className="wrapper">
+          <div className="left">
+            <a href="#intro"> test </a>
+          </div>
+          <h1> Hello </h1>
+          <div className="right">
+            This is right 
+          </div>
+        </div>
+    </div>
+  )
+}
