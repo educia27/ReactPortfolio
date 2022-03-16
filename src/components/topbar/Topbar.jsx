8c57ca96d73223fc8f05cb96aebdd4f5f36d3701
educia27 <edd1114@gmail.com>
2022-03-16 13:46:58 -0500

    progress on portfolio

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,15 +1,29 @@
 import "./topbar.scss"
 
-export default function Topbar() {
+import {Person, Mail} from "@material-ui/icons"
+
+
+export default function Topbar({menuOpen,setMenuOpen}) {
   return (
-    <div className="topbar">
+    <div className={"topbar " + (menuOpen && "active")}>
         <div className="wrapper">
           <div className="left">
-            <a href="#intro"> test </a>
+            <a href="#intro" className="logo"> Eddy T. Garcia </a>
+            <div className="itemContainer">
+              <Person className="icon" />
+              <span> +1 509-387-1569</span>
+            </div>
+            <div className="itemContainer">
+              <Mail className="icon" />
+              <span> edd1114@gmail.com</span>
+            </div>
           </div>
-          <h1> Hello </h1>
           <div className="right">
-            This is right 
+            <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
+              <span className="line1"></span>
+              <span className="line2"></span>
+              <span className="line3"></span>
+            </div>
           </div>
         </div>
     </div>
